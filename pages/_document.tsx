1f85e49fc083b636f2cfab3701237257bdf6ae76
xodud1202 <xodud1202@naver.com>
2026-01-30 17:12:05 +0900

    상품 상세 수정 개발 중 - 카테고리 정보 추가 및 삭제 기능 처리

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
     <Html lang="ko">
       <Head>
         {/* 템플릿 CSS 로딩 */}
-        <link rel="shortcut icon" href="/assets/images/favicon.png" />
+        <link rel="shortcut icon" href="https://image.xodud1202.kro.kr/publist/HDD1/Media/nas/image/common/xodud1202_icon_102x102.png" />
         <link rel="stylesheet" href="/assets/vendors/mdi/css/materialdesignicons.min.css" />
         <link rel="stylesheet" href="/assets/vendors/ti-icons/css/themify-icons.css" />
         <link rel="stylesheet" href="/assets/vendors/css/vendor.bundle.base.css" />
